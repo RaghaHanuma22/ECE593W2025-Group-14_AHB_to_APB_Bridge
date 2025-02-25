2598d31ef2c04fa0623a7850ee0a5405a2defaeb
Raghavendra Davarapalli <ragha@mo.ece.pdx.edu>
2025-02-24 19:23:17 -0800

    Restructuring

--- team_14_AHB2APB_bridge/M4/CLASS/apb_interface.sv
+++ team_14_AHB2APB_bridge/M4/CLASS/apb_interface.sv
@@ -1,48 +1,42 @@
-///////////////////////////////////////////////////////////////////////////////////////////
-// APB Interface
-//
-// Description:
-// This interface provides signals, modports, and clocking blocks for APB protocol,
-// facilitating monitoring and driving of APB components in a verification environment.
-///////////////////////////////////////////////////////////////////////////////////////////
-
+// APB Interface Definition
 interface apb_intf (input logic clk);
-    
-    // APB Signals
-    logic         PRESETn;   // APB reset (active low)
-    logic [31:0]  PADDR;     // APB address
-    logic [31:0]  PWDATA;    // APB write data
-    logic [31:0]  PRDATA;    // APB read data
-    logic         PWRITE;    // APB write enable
-    logic [2:0]   PSELX;     // APB peripheral select
-    logic         PENABLE;   // APB enable
-    
-    // APB MODPORTS
-    modport APB_DRIVER  (clocking apb_driver_cb,  input clk);
+
+    // APB Data and Control Signals
+    bit [31:0]     PRDATA [7:0];  
+    bit [31:0]     PWDATA;        
+    bit [31:0]     PADDR;         
+    bit [7:0]      PSLVERR;       
+    bit [7:0]      PREADY;       
+    bit [7:0]      PSELx;         
+    bit            PENABLE;       
+    bit            PWRITE;
+
+    // Modports to define access restrictions for Driver and Monitor components
+    modport APB_DRIVER  (clocking apb_driver_cb, input clk);
     modport APB_MONITOR (clocking apb_monitor_cb, input clk);
-    
-    // APB Driver Clocking block
+
+    // Clocking Block for APB Driver
     clocking apb_driver_cb @(posedge clk);
-        default input #1 output #1;
-        output PRESETn;
-        output PADDR;
-        output PWDATA;
-        output PWRITE;
-        output PSELX;
-        output PENABLE;
-        input  PRDATA;
+        default input #1 output #1; 
+      
+        output  PRDATA;
+        output  PSLVERR;
+        output  PREADY;
     endclocking
+
     
-    // APB Monitor Clocking Block
     clocking apb_monitor_cb @(posedge clk);
         default input #1 output #1;
-        input PRESETn;
-        input PADDR;
-        input PWDATA;
-        input PWRITE;
-        input PSELX;
-        input PENABLE;
+      
         input PRDATA;
+        input PSLVERR;
+        input PREADY;
+        input PWDATA;
+        input PENABLE;
+        input PSELx;
+        input PADDR;
+        input PWRITE;
     endclocking
-    
+
 endinterface
+
